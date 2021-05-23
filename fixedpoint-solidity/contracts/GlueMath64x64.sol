4e20c026c93b3bb18471e5cff936bbc6c3fce5b3
kozyilmaz <kazim@monolytic.com>
2021-05-23 23:52:05 +0300

    unpack128 added

--- fixedpoint-solidity/contracts/GlueMath64x64.sol
+++ fixedpoint-solidity/contracts/GlueMath64x64.sol
@@ -6,6 +6,7 @@ import "./ABDKMath64x64.sol";
 contract GlueMath64x64 {
 
 	event ValuePacked(int128 output);
+	event ValueUnpacked(uint64 hi, uint64 lo);
 	event ValueMultiplied(int128 output);
 
 	function pack128 (uint64 hi, uint64 lo) public returns (int128) {
@@ -14,10 +15,16 @@ contract GlueMath64x64 {
 		return int128(result);
 	}
 
+	function unpack128 (int128 v) public returns (uint64, uint64) {
+		uint64 hi = (uint64)((uint128(v) & 0xFFFFFFFFFFFFFFFF0000000000000000) >> 64);
+		uint64 lo = (uint64) (uint128(v) & 0xFFFFFFFFFFFFFFFF);
+		emit ValueUnpacked(hi,lo);
+		return (hi,lo);
+	}
+
 	function mul128x128 (int128 x, int128 y) public returns (int128) {
 		int128 result = ABDKMath64x64.mul(x, y);
 		emit ValueMultiplied(result);
 		return result;
 	}
-
 }
