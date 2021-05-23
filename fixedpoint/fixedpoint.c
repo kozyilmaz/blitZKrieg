fa2bb5b851beb7335d2cd1029895c9c0c42b728a
kozyilmaz <kazim@monolytic.com>
2021-05-23 14:47:24 +0300

    fixedpoint added

---
+++ fixedpoint/fixedpoint.c
@@ -0,0 +1,18 @@
+//#define R128_ASSERT(x)
+#define R128_STDC_ONLY
+#define R128_IMPLEMENTATION
+
+#include "r128.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main (int argc, char *argv[])
+{
+	(void) argc;
+	(void) argv;
+
+	return 0;
+}
