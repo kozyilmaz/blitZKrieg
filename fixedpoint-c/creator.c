befcd9f99fd582c1d247e679434e451a82caa006
kozyilmaz <kazim@monolytic.com>
2021-10-19 00:01:12 +0300

    creator

--- fixedpoint-c/creator.c
+++ fixedpoint-c/creator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* generate a random floating point number from min to max */
 double randfrom(double min, double max) 
@@ -11,7 +12,14 @@ double randfrom(double min, double max)
 
 int main (void)
 {
-	double d = randfrom(1,-1);
-	printf("d=%lf\n", d);
+	int i;
+	double d;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 10; i++) {
+		d = randfrom(-1, 1);
+		printf("d[%d] = %.18f\n", i, d);
+	}
 	return 0;
 }
